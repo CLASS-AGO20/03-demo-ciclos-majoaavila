935faf611b2319c38a97f8bfa2db42207d38002e
Majo Ávila Amezcua <mavila7@ucol.mx>
2020-12-03 18:37:29 -0600

    refactored sumarParesFor

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ export default class App {
     sumarParesFor() {
         let suma = 0;
 
-        for(let i = 1; i <= 20; i = i + 1) {
-           if(i % 2 === 0) {
-               suma = suma + i;
-           }
+        for(let i = 0; i <= 20; i = i + 2) {
+           suma = suma + i;
+           
         }
         return suma;
 }
