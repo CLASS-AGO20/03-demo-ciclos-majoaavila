915176e54987a8d16a5a1fb277accb097c24e667
Majo Ávila Amezcua <mavila7@ucol.mx>
2020-12-03 18:20:23 -0600

    implemented sumarParesFor

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-export default class App {}
+export default class App {
+
+    sumarParesFor() {
+        for(let i = 1; i <= 20; i = i + 1) {
+            console.log(i);
+        }
+}
+
+
+}
 
 let app = new App();
+
+app.sumarParesFor();
